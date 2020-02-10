678339b528dc7a768f98201d3e56c434894d9de9
Seike <jseike@8c8590435b44.ant.amazon.com>
2020-02-10 15:08:52 -0300

    adding demo

--- Alexathon/QueroSkill/lambda_function.py
+++ Alexathon/QueroSkill/lambda_function.py
@@ -1,11 +1,3 @@
-
-
-# -*- coding: utf-8 -*-
-
-# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
-# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-# session persistence, api calls, and more.
-# This sample is built using the handler classes approach in skill builder.
 import logging
 import ask_sdk_core.utils as ask_utils
 import os
@@ -14,14 +6,13 @@ import calendar
 from datetime import datetime
 from pytz import timezone
 from ask_sdk_s3.adapter import S3Adapter
-s3_adapter = S3Adapter(bucket_name="amzn1-ask-skill-bb3f3140-4bcd-buildsnapshotbucket-18389gt6kvhvl")
-
-
+s3_adapter = S3Adapter(bucket_name=os.environ["S3_PERSISTENCE_BUCKET"])
 
 from ask_sdk_core.skill_builder import CustomSkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.api_client import DefaultApiClient
 
 from ask_sdk_model import Response
 
@@ -33,41 +24,94 @@ class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-
+        print('ta logando?')
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Olá,sou a Skill da Quero Educação, quando você nasceu?"
-        reprompt_text = "Eu nasci dia quatorze de novembro de dois mil e você?"
+        speak_output = "Olá! Sou a skill do sas. Quando você nasceu?"
+        reprompt_text = "Eu nasci dia 14 de agosto de 2000 e você?"
+
         return (
             handler_input.response_builder
                 .speak(speak_output)
                 .ask(reprompt_text)
                 .response
         )
-    
+
 class HasBirthdayLaunchRequestHandler(AbstractRequestHandler):
     """Handler for launch after they have set their birthday"""
+
     def can_handle(self, handler_input):
         # extract persistent attributes and check if they are all present
         attr = handler_input.attributes_manager.persistent_attributes
-        attributes_are_present = ("ano" in attr and "mes" in attr and "dia" in attr)
+        attributes_are_present = ("year" in attr and "month" in attr and "day" in attr)
 
         return attributes_are_present and ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         attr = handler_input.attributes_manager.persistent_attributes
-        ano = attr['ano']
-        mes = attr['mes'] # month is a string, and we need to convert it to a month index later
-        dia = attr['dia']
-        
-        speak_output = "Bem vindo de volta, eu lembro que nasceu dia {dia} de {mes} de {ano}?".format(dia=dia,mes=mes,ano=ano)
-        
+        year = int(attr['year'])
+        month = attr['month'] # month is a string, and we need to convert it to a month index later
+        day = int(attr['day'])
+
+        # get device id
+        sys_object = handler_input.request_envelope.context.system
+        device_id = sys_object.device.device_id
+
+        # get Alexa Settings API information
+        api_endpoint = sys_object.api_endpoint
+        api_access_token = sys_object.api_access_token
+
+        # construct systems api timezone url
+        url = '{api_endpoint}/v2/devices/{device_id}/settings/System.timeZone'.format(api_endpoint=api_endpoint, device_id=device_id)
+        headers = {'Authorization': 'Bearer ' + api_access_token}
+
+        userTimeZone = ""
+        try:
+	        r = requests.get(url, headers=headers)
+	        res = r.json()
+	        logger.info("Device API result: {}".format(str(res)))
+	        userTimeZone = res
+        except Exception:
+	        handler_input.response_builder.speak("Ocorreu um problema ao conectar ao serviço")
+	        return handler_input.response_builder.response
+
+        # getting the current date with the time
+        now_time = datetime.now(timezone(userTimeZone))
+
+        # Removing the time from the date because it affects our difference calculation
+        now_date = datetime(now_time.year, now_time.month, now_time.day)
+        current_year = now_time.year
+
+        # getting the next birthday
+        month_as_index = list(calendar.month_abbr).index(month[:3])
+        next_birthday = datetime(current_year, month_as_index, day)
+
+        # check if we need to adjust bday by one year
+        if now_date > next_birthday:
+            next_birthday = datetime(
+                current_year + 1,
+                month_as_index,
+                day
+            )
+            current_year += 1
+        # setting the default speak_output to Happy xth Birthday!!
+        # alexa will automatically correct the ordinal for you.
+        # no need to worry about when to use st, th, rd
+        speak_output = "Feliz aniversário de {} anos".format(str(current_year - year))
+        if now_date != next_birthday:
+            diff_days = abs((now_date - next_birthday).days)
+            speak_output = "Bem vindo de volta. Parece que faltam \
+                            {days} dias até seu aniversário de  {birthday_num}\
+                            anos".format(
+                                days=diff_days,
+                                birthday_num=(current_year-year)
+                            )
+
         handler_input.response_builder.speak(speak_output)
 
         return handler_input.response_builder.response
-
 
 class CaptureBirthdayIntentHandler(AbstractRequestHandler):
     """Handler for Hello World Intent."""
@@ -78,20 +122,23 @@ class CaptureBirthdayIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         slots = handler_input.request_envelope.request.intent.slots
-        ano = slots["ano"].value
-        mes = slots["mes"].value
-        dia = slots["dia"].value
+        year = slots["year"].value
+        month = slots["month"].value
+        day = slots["day"].value
+
         attributes_manager = handler_input.attributes_manager
+
         birthday_attributes = {
-            "ano": ano,
-            "mes": mes,
-            "dia": dia
+            "year": year,
+            "month": month,
+            "day": day
         }
+
         attributes_manager.persistent_attributes = birthday_attributes
         attributes_manager.save_persistent_attributes()
-        
-        speak_output = "Ok! Vou me lembrar que você nasceu dia {dia} de {mes} de {ano}".format(ano=ano,mes=mes,dia=dia)
-        
+
+        speak_output = 'Obrigado, vou lembrar que nasceu dia {day} de {month} de {year}.'.format(month=month, day=day, year=year)
+
         return (
             handler_input.response_builder
                 .speak(speak_output)
@@ -127,7 +174,7 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Goodbye!"
+        speak_output = "Adeus!"
 
         return (
             handler_input.response_builder
@@ -186,7 +233,7 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
         # type: (HandlerInput, Exception) -> Response
         logger.error(exception, exc_info=True)
 
-        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+        speak_output = "Desculpe, não entendi, pode repetir?."
 
         return (
             handler_input.response_builder
@@ -200,7 +247,7 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 # defined are included below. The order matters - they're processed top to bottom.
 
 
-sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
+sb = CustomSkillBuilder(persistence_adapter=s3_adapter, api_client=DefaultApiClient())
 
 sb.add_request_handler(HasBirthdayLaunchRequestHandler())
 sb.add_request_handler(LaunchRequestHandler())
